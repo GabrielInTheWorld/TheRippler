adbd924018a388d26fd271b3dd24d5cb9f181d62
Gabriel Meyer <meyergabriel@live.de>
2019-11-14 13:38:29 +0100

    First implement of canvas

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TheRippler.Source.Collections;
 
 namespace TheRippler {
     /// <summary>
@@ -23,8 +25,90 @@ namespace TheRippler {
         private enum DrawShape {
             Line, Rectangle, Ellipse
         }
+
+        private DrawShape selectedShape = DrawShape.Line;
+        private Point startPointer;
+        private Point movePointer;
+        private Point endPointer;
+
+        private DataVector<UIElement> elementStack = new DataVector<UIElement>();
+
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void Draw() {
+            switch (this.selectedShape) {
+                case DrawShape.Line:
+                    this.ToCanvas(this.DrawLine(this.endPointer.X, this.endPointer.Y));
+                    break;
+                case DrawShape.Rectangle:
+                    this.ToCanvas(this.DrawRectangle());
+                    break;
+            }
+        }
+
+        private void DrawPreview() {
+            switch(this.selectedShape) {
+                case DrawShape.Line:
+                    this.ToPreviewCanvas(this.DrawLine(this.movePointer.X, this.movePointer.Y));
+                    break;
+                case DrawShape.Rectangle:
+                    this.ToPreviewCanvas(this.DrawRectangle());
+                    break;
+            }
+        }
+
+        private Line DrawLine(double endX, double endY) {
+            return new Line() {
+                Stroke = Brushes.Blue,
+                X1 = this.startPointer.X,
+                Y1 = this.startPointer.Y,
+                X2 = endX,
+                Y2 = endY
+            };
+        }
+
+        private Rectangle DrawRectangle() {
+            return new Rectangle() {
+                Stroke = Brushes.Blue,
+                StrokeThickness = 4
+            };
+        }
+
+        private void ToCanvas(UIElement element) {
+            CanvasDraw.Children.Add(element);
+        }
+
+        private void ToPreviewCanvas(UIElement element) {
+            CanvasPreview.Children.Clear();
+            CanvasPreview.Children.Add(element);
+        }
+
+        private void DrawPencil(object sender, RoutedEventArgs e) {
+            this.selectedShape = DrawShape.Line;
+        }
+
+        private void DrawRectangle(object sender, RoutedEventArgs e) {
+            this.selectedShape = DrawShape.Rectangle;
+        }
+
+        private void CanvasMouseDown(object sender, MouseButtonEventArgs e) {
+            this.startPointer = e.GetPosition(this);
+        }
+
+        private void CanvasMouseUp(object sender, MouseButtonEventArgs e) {
+            this.endPointer = e.GetPosition(this);
+            this.Draw();
+        }
+
+        private void CanvasMouseMove(object sender, MouseEventArgs e) {
+            Console.WriteLine("mouse move");
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                Console.WriteLine("Butten pressed");
+                this.movePointer = e.GetPosition(this);
+                this.DrawPreview();
+            }
         }
     }
 }
---
+++ Source/Collections/DataVector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheRippler.Source.Collections {
+    class DataVector<T> {
+        private T[] stack;
+        private int incrementSize = 0;
+        private int _size = 0;
+
+        public int Size { get;  }
+
+        public DataVector() {
+            this.stack = new T[2];
+        }
+
+        public DataVector(int startSize) {
+            this.stack = new T[startSize];
+        }
+
+        public DataVector(int startSize, int incrementSize) {
+            this.stack = new T[startSize];
+            this.incrementSize = incrementSize;
+        }
+
+        public void Add(T element) {
+            if (this.size == this.stack.Length) {
+                this.Resize();
+            }
+            this.stack[this._size++] = element;
+        }
+
+        private void Resize() {
+            T[] tmp = new T[this._size];
+            if (this.incrementSize == 0) {
+                this.stack = new T[this._size * 2];
+            } else {
+                this.stack = new T[this._size + this.incrementSize];
+            }
+            for (int i = 0; i < tmp.Length; ++i) {
+                this.stack[i] = tmp[i];
+            }
+        }
+    }
+}
